ea4343941aaf90ef9ce8c32ea2466dc65f95a5bc
Mehmet Aktay <71221185+semdin@users.noreply.github.com>
2025-06-22 22:29:01 +0300

    Initial commit

---
+++ src/main/java/com/ecommerce/sellerx/entities/User.java
@@ -0,0 +1,35 @@
+package com.ecommerce.sellerx.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "name = " + name + ", " +
+                "email = " + email + ")";
+    }
+}
